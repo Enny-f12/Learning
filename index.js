2c4595e6173d118b7bd33155d3f8bdeeb0d6241a
Alayande Esther <alayandeesther469@gmail.com>
2025-12-19 01:37:52 +0100

    Exercise 3.5 completed

--- index.js
+++ index.js
@@ -1,75 +1,60 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-let notes = [
+let persons = [
   {
     id: "1",
-    content: "HTML is easy",
-    important: true
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
     id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    
   },
   {
     id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    name: "Dan Abramov",
+    number: "12-43-234345",
+
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    
+  }                             
+    
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Exercise 3.5</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
- response.json(notes)
+app.get('/api/persons', (request, response) => {
+ response.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
 
-   if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  } 
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
+  const randomId = Math.floor(Math.random() * 1000000)
+  return String(randomId)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
+  
+  const person = {
+    name: body.name,
+    number: body.number,
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
 const PORT = 3001
