74dfdefc06916549098a1556625bba3e890d0759
Alayande Esther <alayandeesther469@gmail.com>
2025-12-23 16:42:05 +0100

    commit

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+  persons = persons.find(person => person.id !== id)
 
   response.status(204).end()
 })
