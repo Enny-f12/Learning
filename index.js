962429d8a84cbb0977bef22aa5d872e1c89e0819
Alayande Esther <alayandeesther469@gmail.com>
2026-01-12 15:35:57 +0100

    Use PORT from environment for deployment

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = http.createServer((request, response) => {
   response.end('Hello World')
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
