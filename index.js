d15c634fbc4127504026be07730cf006bf1651db
Alayande Esther <alayandeesther469@gmail.com>
2026-01-17 03:46:10 +0100

    exercise 8

--- index.js
+++ index.js
@@ -2,32 +2,87 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+
+
+morgan.token('body', (request, response) => {
+  return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+let persons = [
+    {
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+
+    },
+    {
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345",
+
+    },
+    {
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+
+    }
+
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Implementing morgan using tiny format</h1>')
+    response.send('<h1>Exercise 3.5</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.send(notes)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const name = body.name.trim()
+
+    if (!name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const formattedName = body.name
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ')
+
+    const nameExists = persons.find(p => p.name === formattedName)
+
+    if (nameExists) {
+        return response.status(400).json({ error: 'name must be unique, add full name' })
+    }
+
+    const numberExists = persons.find(p => p.number === body.number)
+
+    if (numberExists) {
+        return response.status(400).json({ error: 'number must be unique, add another number' })
+    }
+
+    const person = {
+        name: formattedName,
+        number: body.number,
+        id: String(Math.floor(Math.random() * 1000000))
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 
 
 
