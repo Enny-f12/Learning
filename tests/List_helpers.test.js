70e854268e3a5270c27402c057ac1e685f59be18
Alayande Esther <alayandeesther469@gmail.com>
2026-03-26 12:45:26 +0100

    add totalLikes logic and nested test descriptions (Exercise 4.4)

--- tests/List_helpers.test.js
+++ tests/List_helpers.test.js
@@ -1,13 +1,33 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
-
 const listHelper = require('../utils/List_helpers')
 
-test('dummy returns one', () => {
-  const blogs = [] // created an empty list to pass in
+describe('total likes', () => {
+  // Test 1: Empty list
+  test('of empty list is zero', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
 
-  const result = listHelper.dummy(blogs)
+  // Test 2: One blog
+  test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    ]
+    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
+  })
 
+  // Test 3: Multiple blogs
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      { title: 'Blog 1', likes: 10 },
+      { title: 'Blog 2', likes: 5 },
+      { title: 'Blog 3', likes: 12 }
+    ]
 
-  assert.strictEqual(result, 1)
+    assert.strictEqual(listHelper.totalLikes(blogs), 27)
+  })
 })
--- utils/List_helpers.js
+++ utils/List_helpers.js
@@ -1,8 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => {
+const dummy = (_blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  // If the list is empty, return 0
+  // Otherwise, add up all the likes
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
